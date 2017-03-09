8ad27265971a7292851775c30c5b804047cdc095
waikei1917 <waikei1917oct@gmail.com>
2017-03-08 20:05:15 -0700

    Print and Variables

--- src/com/JavaBeginnerTube/Main.java
+++ src/com/JavaBeginnerTube/Main.java
@@ -4,6 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.print("Hello World!!!");
+        System.out.println("Hello World!!!");
+
+        double tuna;
+        tuna = 5.28;
+        System.out.print("There are ");
+        System.out.print(tuna);
+        System.out.println(" Moives");
+
+        System.out.print("Apples");
+
     }
 }
